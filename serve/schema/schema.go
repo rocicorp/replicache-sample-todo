e953aef4c39cebd665b0baecf4d979bcd335dc9d
Aaron Boodman <aaron@aaronboodman.com>
2020-03-06 23:00:51 -1000

    WIP: Setting up schema in demo server. For some reason SQL error on 'show databases'.

--- serve/schema/schema.go
+++ serve/schema/schema.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,23 +11,114 @@ import (
 )
 
 const (
-	aws_access_key_id     = "REPLICANT_AWS_ACCESS_KEY_ID"
-	aws_secret_access_key = "REPLICANT_AWS_SECRET_ACCESS_KEY"
-	aws_region            = "us-west-2"
+	awsAccessKeyId     = "REPLICANT_AWS_ACCESS_KEY_ID"
+	awsSecretAccessKey = "REPLICANT_AWS_SECRET_ACCESS_KEY"
+	awsRegion          = "us-west-2"
+	schemaVersion      = 1
 )
+
+var (
+	resourceArn = aws.String("arn:aws:rds:us-west-2:712907626835:cluster:replicache-demo-notes")
+	secretArn   = aws.String("arn:aws:secretsmanager:us-west-2:712907626835:secret:rds-db-credentials/cluster-X5NALMLWZ34K55M5ZZVPN2IYOI/admin-65L3ia")
+)
+
+func dbName() (string, error) {
+	n := "REPLICANT_SAMPLE_TODO_ENV"
+	env := os.Getenv(n)
+	if env == "" {
+		return "", fmt.Errorf("Required environment variable %s not found", n)
+	} else {
+		return fmt.Sprintf("replicache_sample_todo__%s", env), nil
+	}
+}
 
 func Create() error {
 	sess := session.Must(session.NewSession(
-		aws.NewConfig().WithRegion(aws_region).WithCredentials(
-				// Have to do this wackiness because not allowed to set AWS env variables in Now for some reason.
-				credentials.NewStaticCredentials(
-						os.Getenv(aws_access_key_id),
-						os.Getenv(aws_secret_access_key), ""))))
+		aws.NewConfig().WithRegion(awsRegion).WithCredentials(
+			// Have to do this wackiness because not allowed to set AWS env variables in Now for some reason.
+			credentials.NewStaticCredentials(
+				os.Getenv(awsAccessKeyId),
+				os.Getenv(awsSecretAccessKey), ""))))
 	svc := rdsdataservice.New(sess)
-	_, err := svc.ExecuteStatement(&rdsdataservice.ExecuteStatementInput{
-		ResourceArn: aws.String("arn:aws:rds:us-west-2:712907626835:cluster:replicache-demo-notes"),
-		SecretArn: aws.String("arn:aws:secretsmanager:us-west-2:712907626835:secret:rds-db-credentials/cluster-X5NALMLWZ34K55M5ZZVPN2IYOI/admin-65L3ia"),
-		Sql: aws.String("CREATE DATABASE foo"),
+
+	name, err := dbName()
+	if err != nil {
+		return err
+	}
+
+	beginTransactionOutput, err := svc.BeginTransaction(&rdsdataservice.BeginTransactionInput{
+		ResourceArn: resourceArn,
+		SecretArn:   secretArn,
 	})
-	return err
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_, err = svc.RollbackTransaction(&rdsdataservice.RollbackTransactionInput{
+			ResourceArn:   resourceArn,
+			SecretArn:     secretArn,
+			TransactionId: beginTransactionOutput.TransactionId,
+		})
+		if err != nil {
+			panic(fmt.Errorf("RollbackTransaction failed: %s", err))
+		}
+	}()
+
+	execStatementOutput, err := exec(svc, fmt.Sprintf("SHOW DATABASES LIKE '%s'", name))
+	if err != nil {
+		return err
+	}
+
+	if len(execStatementOutput.Records) == 1 {
+		_, err = exec(svc, fmt.Sprintf("USE DATABASE %s", name))
+		if err != nil {
+			return err
+		}
+		execStatementOutput, err := exec(svc, "SELECT Version FROM Meta LIMIT 1")
+		if err != nil {
+			return err
+		}
+		if len(execStatementOutput.Records) == 1 && *(execStatementOutput.Records[0][0].LongValue) == schemaVersion {
+			return nil
+		}
+	}
+
+	statements := []string{
+		fmt.Sprintf("DROP DATABASE IF EXISTS %s", name),
+		fmt.Sprintf("CREATE DATABASE %s", name),
+		fmt.Sprintf("USE DATABASE %s", name),
+		fmt.Sprintf("CREATE TABLE Meta (Version) Values (%d)", schemaVersion),
+		"CREATE TABLE User (Id INT PRIMARY KEY)",
+	}
+
+	for _, s := range statements {
+		_, err = exec(svc, s)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = svc.CommitTransaction(&rdsdataservice.CommitTransactionInput{
+		ResourceArn:   resourceArn,
+		SecretArn:     secretArn,
+		TransactionId: beginTransactionOutput.TransactionId,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func exec(svc *rdsdataservice.RDSDataService, sql string) (*rdsdataservice.ExecuteStatementOutput, error) {
+	fmt.Printf("Executing: %s\n", sql)
+	out, err := svc.ExecuteStatement(&rdsdataservice.ExecuteStatementInput{
+		ResourceArn: resourceArn,
+		SecretArn:   secretArn,
+		Sql:         aws.String(sql),
+	})
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	return out, err
 }
