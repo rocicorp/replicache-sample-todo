2a82028e0e0834d632302585c912e180f76d44d0
Aaron Boodman <aaron@aaronboodman.com>
2020-03-17 23:29:01 -1000

    Version schema by hash instead of version number.
    Since this is demo data, for now we can just drop entire db when schema changes.

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module roci.dev/replicache-sample-todo
 go 1.12
 
 require (
+	github.com/aws/aws-lambda-go v1.15.0 // indirect
 	github.com/aws/aws-sdk-go v1.29.19
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dhruvbird/go-cowsay v0.0.0-20131019225157-6fd7bd0281c0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.4.0
+	github.com/zeit/now v0.0.0-20200317230807-4208dc0466b1 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- serve/model/schema/schema.go
+++ serve/model/schema/schema.go
@@ -1,32 +1,37 @@
 package schema
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"strings"
 
 	"roci.dev/replicache-sample-todo/serve/db"
 )
 
-const (
-	schemaVersion = 4
-)
-
 func Create(db *db.DB, name string) (err error) {
-	execStatementOutput, err := db.Exec(fmt.Sprintf("SELECT IntVal FROM %s.Meta WHERE Name = 'Version' LIMIT 1", name))
-	if err != nil {
-		fmt.Printf("ERROR: Invalid database: %s\n", err)
-	} else if len(execStatementOutput.Records) == 1 && *(execStatementOutput.Records[0][0].LongValue) == schemaVersion {
-		return nil
-	}
-
 	statements := []string{
 		fmt.Sprintf("DROP DATABASE IF EXISTS %s", name),
 		fmt.Sprintf("CREATE DATABASE %s", name),
-		fmt.Sprintf("CREATE TABLE %s.Meta (Name VARCHAR(16) PRIMARY KEY NOT NULL, IntVal INT)", name),
-		fmt.Sprintf("INSERT INTO %s.Meta Values ('Version', %d)", name, schemaVersion),
+		fmt.Sprintf("CREATE TABLE %s.Meta (Name VARCHAR(255) PRIMARY KEY NOT NULL, Value VARCHAR(255) NOT NULL)", name),
 		fmt.Sprintf("CREATE TABLE %s.User (Id INT PRIMARY KEY NOT NULL)", name),
 		fmt.Sprintf("CREATE TABLE %s.TodoList (Id INT PRIMARY KEY NOT NULL, OwnerUserId INT NOT NULL, FOREIGN KEY (OwnerUserId) REFERENCES %s.User (Id))", name, name),
 		fmt.Sprintf("CREATE TABLE %s.Todo (Id INT PRIMARY KEY NOT NULL, TodoListId INT NOT NULL, Title VARCHAR(255) NOT NULL, Complete BIT NOT NULL, SortOrder FLOAT(53) NOT NULL, FOREIGN KEY (TodoListId) REFERENCES %s.TodoList (Id))", name, name),
 	}
+
+	schemaHash := sha1.Sum([]byte(strings.Join(statements, "\n")))
+	schemaHashStr := hex.EncodeToString(schemaHash[:])
+
+	execStatementOutput, err := db.Exec(fmt.Sprintf("SELECT Value FROM %s.Meta WHERE Name = 'Version' AND Value = '%s' LIMIT 1", name, schemaHashStr))
+	if err != nil {
+		fmt.Printf("ERROR: Dropping invalid database: %s\n", err)
+	} else if len(execStatementOutput.Records) == 1 {
+		return nil
+	}
+
+	fmt.Printf("Dropping database with old schema...")
+
+	statements = append(statements, fmt.Sprintf("INSERT INTO %s.Meta (Name, Value) VALUES ('Version', '%s')", name, schemaHashStr))
 
 	for _, s := range statements {
 		_, err = db.Exec(s)
