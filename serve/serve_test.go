17688b1306175f4dec69789778d763d781f3ceaa
Aaron Boodman <aaron@aaronboodman.com>
2020-05-21 14:59:50 -1000

    Add server push via FCM (#10)

--- serve/serve_test.go
+++ serve/serve_test.go
@@ -1,8 +1,12 @@
 package serve
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strconv"
 	"testing"
 
@@ -48,4 +52,68 @@ func TestAuthenticate(t *testing.T) {
 		assert.Equal(t.wantUserID, userID)
 		assert.Equal(t.wantCode, w.Result().StatusCode)
 	}
+}
+
+type doer struct {
+	gotReq *http.Request
+}
+
+func (d *doer) Do(r *http.Request) (*http.Response, error) {
+	d.gotReq = r
+	return nil, nil
+}
+func TestPoke(t *testing.T) {
+	assert := assert.New(t)
+
+	db := db.New()
+	_, err := db.Exec("DROP DATABASE IF EXISTS test", nil)
+	assert.NoError(err)
+	err = schema.Create(db, "test")
+	assert.NoError(err)
+
+	db.Use("test")
+
+	userID, err := user.Create(db, "foo@foo.com")
+	assert.NoError(err)
+
+	os.Setenv("FCM_SERVER_KEY", "test_server_key")
+
+	type tc struct {
+		path     string
+		wantPoke bool
+		// body doesn't matter because serve is lazy and pokes clients
+		// just for receiving a request, even if nothing is changed.
+	}
+
+	f := func(tt tc) {
+		r, err := http.NewRequest("POST", tt.path, &bytes.Buffer{})
+		r.Header.Add("Authorization", fmt.Sprintf("%d", userID))
+		assert.NoError(err)
+
+		w := httptest.NewRecorder()
+		d := &doer{}
+		impl(w, r, db, d)
+
+		if !tt.wantPoke {
+			assert.Nil(d.gotReq)
+			return
+		}
+
+		body := &bytes.Buffer{}
+		_, err = io.Copy(body, d.gotReq.Body)
+		assert.NoError(err)
+
+		assert.Equal("POST", d.gotReq.Method)
+		assert.Equal("https://fcm.googleapis.com/fcm/send", d.gotReq.URL.String())
+		assert.Equal(fmt.Sprintf(`{"data": {},  "to": "/topics/u-%d"}`, userID), body.String())
+		assert.Equal("application/json", d.gotReq.Header.Get("Content-type"))
+		assert.Equal("Bearer test_server_key", d.gotReq.Header.Get("Authorization"))
+	}
+
+	f(tc{path: "/serve/replicache-batch", wantPoke: true})
+	f(tc{path: "/serve/replicache-client-view", wantPoke: false})
+	f(tc{path: "/serve/list-create", wantPoke: true})
+	f(tc{path: "/serve/todo-create", wantPoke: true})
+	f(tc{path: "/serve/todo-update", wantPoke: true})
+	f(tc{path: "/serve/todo-delete", wantPoke: true})
 }
